4642c16b803d66dc9dbebd9cf0d018d1201e59af
Ajay Thorve <ajaythorve1994@gmail.com>
2022-10-14 11:19:41 -0700

    remove unused imports

--- src/components/ruler.js
+++ src/components/ruler.js
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/components/ruler.module.css";
 
 function Ruler({ mean, score }) {
