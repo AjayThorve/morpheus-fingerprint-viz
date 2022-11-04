99f9c2b794c638bca6581b81d62912b518654e2d
Ajay Thorve <ajaythorve1994@gmail.com>
2022-11-04 08:54:31 -0700

    merge util functions in a single file

--- src/components/server/utils.js
+++ src/components/server/utils.js
@@ -9,7 +9,8 @@ import {
   Uint64,
   TimestampSecond,
 } from "@rapidsai/cudf";
-import { mapValuesToColorSeries } from "../utils";
+const D3Node = require("d3-node");
+const d3 = new D3Node().d3; // initializes D3 with container element
 
 export async function sendDF(df, res) {
   await pipeline(
@@ -479,4 +480,77 @@ export function generateData(
       .interleaveColumns()
       .cast(new Float32()),
   });
+}
+
+function hexToRgb(rgb) {
+  var result = d3.color(rgb);
+  return result
+    ? {
+        r: result.r / 255,
+        g: result.g / 255,
+        b: result.b / 255,
+      }
+    : null;
+}
+
+export function mapValuesToColorSeries(
+  values,
+  domain,
+  colors_,
+  nullColor = { r: 33, g: 33, b: 33 }
+) {
+  // validate colors and domain lengths
+  if (colors_.length < 1 || domain.length < 1) {
+    throw new Error("colors and domain must be arrays of length 1 or greater");
+  }
+  const colors = d3
+    .scaleSequential()
+    .interpolator(d3.interpolateRgb(colors_[0], colors_[1]))
+    .domain([domain[0] * 100, domain[1] * 100]);
+
+  const color_r = Series.sequence({
+    step: 0,
+    init: nullColor.r / 255,
+    type: new Float32(),
+    size: values.length,
+  });
+  const color_g = Series.sequence({
+    step: 0,
+    init: nullColor.g / 255,
+    type: new Float32(),
+    size: values.length,
+  });
+  const color_b = Series.sequence({
+    step: 0,
+    init: nullColor.b / 255,
+    type: new Float32(),
+    size: values.length,
+  });
+
+  const colorIndices = Series.sequence({
+    type: new Uint32(),
+    init: 0,
+    step: 1,
+    size: values.length,
+  });
+
+  if (domain.length == 1) {
+    const boolMask = values.ge(domain[0]);
+    const indices = colorIndices.filter(boolMask);
+    const color = hexToRgb(colors(domain[0] * 100));
+    color_r.setValues(indices, color.r);
+    color_g.setValues(indices, color.g);
+    color_b.setValues(indices, color.b);
+  } else {
+    for (let i = domain[0]; i < domain[1]; i += domain[2]) {
+      const boolMask = values.ge(i);
+      const indices = colorIndices.filter(boolMask);
+      const color = hexToRgb(colors(i * 100));
+      color_r.setValues(indices, color.r);
+      color_g.setValues(indices, color.g);
+      color_b.setValues(indices, color.b);
+    }
+  }
+
+  return { color_r, color_g, color_b };
 }
